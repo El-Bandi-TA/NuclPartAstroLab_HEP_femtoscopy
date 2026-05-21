a5e0d41ecc9c48660c41f12f06014579023a7de5
Toronyi András <andras.toronyi@gmail.com>
2026-05-21 12:08:40 +0200

    feat(fit): minos err, fit range
    - impelemnt optional flag to calculate minos error
    - implement reading of different fit ranges for different histograms from a config file

--- apps/fitCQ.cpp
+++ apps/fitCQ.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 
 CoulCorrCalc *cccinstance;
@@ -49,7 +50,11 @@ struct FitResults {
 
     void print_results();
     void print_status();
-    void write_results(std::ostream& outfile, double KTmin, double KTmax);
+    void write_results(
+        std::ostream& outfile,
+        double KTmin, double KTmax,
+        double Qmin, double Qmax
+    );
 };
 
 std::string get_current_time();
@@ -59,7 +64,9 @@ double MyChi2(
     const double *par, const std::vector<double>& Qvals,
     const std::vector<double>& expvals, const std::vector<double>& experrs
 );
-FitResults fit_CoulCorrLevy(TH1D* hist, double Qmin, double Qmax);
+FitResults fit_CoulCorrLevy(
+    TH1D* hist, double Qmin, double Qmax, bool calcMinos
+);
 
 void write_header(std::ostream& out_file);
 
@@ -76,10 +83,10 @@ int main(int argc, char** argv) {
     std::string outFilename = (
         projRoot+"/data/fitparams_"+get_current_time()+".txt"
     );
+    int mergeQBins = 1;
     int startFrom = 0;
     int fitN = 0;
-    double Qmin = 0.025;
-    double Qmax = 0.25;
+    bool calcMinos = false;
 
     for (int i = 1; i < argc; i++) {
         std::string arg = argv[i];
@@ -90,35 +97,39 @@ int main(int argc, char** argv) {
                       << "inHistFile=HEPQC_ROOT/data/Q_full/Q_plus.root] "
                       << "[-o|--output outFilename="
                       << "HEPQC_ROOT/data/fitparams_CURRENTTIME.txt] "
+                      << "[-b|--merge-bins mergeQBins=1]"
                       << "[-n|--fitN fitN=0] "
                       << "[-s|--start-from startFrom=0] "
-                      << "[--Qmin Qmin=0.025] "
-                      << "[--Qmax Qmax=0.25] "
+                      << "[-m|--calc-minos calcMinos=false]"
                       << std::endl;
             return -1;
         } else if ((arg == "-i" || arg == "--input") && i+1 < argc) {
             inHistFile = argv[++i];
         } else if ((arg == "-o" || arg == "--output") && i+1 < argc) {
             outFilename = argv[++i];
+        } else if ((arg == "-b" || arg == "--merge-bins")) {
+            mergeQBins = std::atoi(argv[++i]);
         } else if ((arg == "-s" || arg == "--start-from") && i+1 < argc) {
             startFrom = std::atoi(argv[++i]);
             if (startFrom < 0) startFrom = 0;
         } else if ((arg == "-n" || arg == "--fitN")) {
             fitN = std::atoi(argv[++i]);
             if (fitN < 0) fitN = 0;
-        } else if (arg == "--Qmin") {
-            Qmin = std::stod(argv[++i]);
-        } else if (arg == "--Qmax") {
-            Qmax = std::stod(argv[++i]);
+        } else if ((arg == "-m" || arg == "--calc-minos")) {
+            calcMinos = true;
         }
     }
+
+    std::vector<double> Qmin {};
+    std::vector<double> Qmax {};
+
     // --- Parsing finished ---
 
     // Create output file and check wether it is valid
     std::ofstream outParamFile(outFilename);
 
     if (!outParamFile.is_open()) {
-        std::cerr << "FATAL: Could not open output file" << outFilename
+        std::cerr << "FATAL: Could not open output file " << outFilename
                   << "for writing\n";
         return 1;
     }
@@ -130,24 +141,53 @@ int main(int argc, char** argv) {
         inHistFile,
         "Q", "(Q); Q [GeV]; Counts"
     );
+    // KThists.merge_hists(0,3);
+    if (mergeQBins>1) {
+        KThists.merge_hist_bins(mergeQBins);
+        KThists.scale_ABhists();
+        KThists.create_Chists();
+    };
 
     // Initialize Coulomb Fourier calculation instance
     cccinstance = new CoulCorrCalc();
 
+    // Calculate how many to fit
     int fit = KThists.num_KT_bins-startFrom;
     if (fitN>0 && fitN<fit) fit = fitN;
 
+    // Parse Q fit range config file
+    std::string QfitrangeConfigFilename = projRoot+"/config/Qfitrange.txt";
+    std::ifstream QfitrangeConfig(QfitrangeConfigFilename);
+    if (!QfitrangeConfig.is_open()) {
+        std::cerr << "FATAL: Could not open Q fit range config file "
+                  << QfitrangeConfigFilename << ". Aborting...";
+        return 1;
+    }
+    double qmin, qmax;
+    int bin;
+    std::string line {};
+    while (std::getline(QfitrangeConfig,line)) {
+        if (line[0]=='#') continue;
+        std::stringstream ss {line};
+        ss >> bin >> qmin >> qmax;
+        Qmin.push_back(qmin);
+        Qmax.push_back(qmax);
+    }
+
+    // Doing fits
     for(int i = startFrom; i < fit+startFrom; i++) {
-        std::cerr << "Fitting hist " << i << "...\n";
+        std::cerr << "Fitting hist " << i << " between " << Qmin.at(i) << " GeV"
+                  << " and " << Qmax.at(i) << " GeV...\n";
         FitResults res = fit_CoulCorrLevy(
-            KThists.Chists.at(i), Qmin, Qmax
+            KThists.Chists.at(i), Qmin.at(i), Qmax.at(i), calcMinos
         );
         res.print_results();
         std::cerr << "-------------------------------------------" << std::endl;
         res.write_results(
             outParamFile,
             KThists.KT_bin_edges.at(i),
-            KThists.KT_bin_edges.at(i+1)
+            KThists.KT_bin_edges.at(i+1),
+            Qmin.at(i), Qmax.at(i)
         );
     }
 
@@ -182,7 +222,9 @@ double MyChi2(
     return chi2;
 }
 
-FitResults fit_CoulCorrLevy(TH1D* hist, double Qmin, double Qmax) {
+FitResults fit_CoulCorrLevy(
+    TH1D* hist, double Qmin, double Qmax, bool calcMinos
+) {
     std::vector<double> Q_values;
     std::vector<double> exp_values;
     std::vector<double> exp_errors;
@@ -226,9 +268,13 @@ FitResults fit_CoulCorrLevy(TH1D* hist, double Qmin, double Qmax) {
     // Set the free variables to be minimized
     // start from completely out of context values to test fit accuracy
     min.SetVariable(0,"N",     0.95,0.01);
-    min.SetVariable(1,"lambda",0.20,0.01);
+    min.SetVariable(1,"lambda",1.00,0.01);
     min.SetVariable(2,"R",     9.20,0.01);
-    min.SetVariable(3,"alpha", 1.70,0.01);
+    min.SetVariable(3,"alpha", 1.20,0.01);
+    // min.SetLowerLimitedVariable(0,"N",0.95,0.01,0);
+    // min.SetVariable(1,"lambda",1.00,0.01);
+    // min.SetLowerLimitedVariable(2,"R", 7.50,0.01,0);
+    // min.SetLimitedVariable(3,"alpha", 1.30,0.01,1,2);
 
     // min.SetFixedVariable(0,"x0",91);
     
@@ -248,12 +294,16 @@ FitResults fit_CoulCorrLevy(TH1D* hist, double Qmin, double Qmax) {
         res.params.at(i) = min.X()[i];
         res.errors.at(i) = min.Errors()[i];
         
-        // double errdn;
-        // double errup;
-        // if (min.GetMinosError(i,errdn,errup)) {
-        //     res.minosMinus.at(i) = errdn;
-        //     res.minosPlus.at(i) = errup;
-        // }
+        if (calcMinos) {
+            std::cerr << "Calculating minos error for parameter "
+                      << i << "...\n";
+            double errdn;
+            double errup;
+            if (min.GetMinosError(i,errdn,errup)) {
+                res.minosMinus.at(i) = errdn;
+                res.minosPlus.at(i) = errup;
+            }
+        }
     }
 
     res.fitstatus = min.Status();
@@ -266,7 +316,7 @@ FitResults fit_CoulCorrLevy(TH1D* hist, double Qmin, double Qmax) {
 
 void write_header(std::ostream& out_file) {
     out_file << "#Params: N, lambda, R, alpha\n";
-    out_file << "#KTmin\tKTmax\tSuccess\t";
+    out_file << "#KTmin\tKTmax\tQmin\tQmax\tSuccess\t";
     for(int i = 0; i < NPARS; i++) {
         out_file << "Par" << i << "\t";
         out_file << "Err" << i << "\t";
@@ -309,9 +359,10 @@ void FitResults::print_results() {
 }
 
 void FitResults::write_results(
-    std::ostream& outfile, double KTmin, double KTmax
+    std::ostream& outfile, double KTmin, double KTmax, double Qmin, double Qmax
 ) {
-    outfile << '\n' << KTmin << '\t' << KTmax << '\t' << success << '\t';
+    outfile << '\n' << KTmin << '\t' << KTmax << '\t'
+            << Qmin << '\t' << Qmax << '\t' << success << '\t';
     for(int i = 0; i < NPARS; i++) {
         outfile << params.at(i) << "\t";
         outfile << errors.at(i) << "\t";
